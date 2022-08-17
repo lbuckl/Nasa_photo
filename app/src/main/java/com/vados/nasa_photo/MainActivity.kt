cee510282a5677c37394970523f398ac88ba2f45
Molchanov <ya_mvn@mail.ru>
2022-08-17 11:22:28 +0500

    Initial commit

---
+++ app/src/main/java/com/vados/nasa_photo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vados.nasa_photo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vados.nasa_photo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
