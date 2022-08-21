0e1deea44bcc4d450d8f87aee0463ad056953094
lbuckl <ya_mvn@mail.ru>
2022-08-21 14:38:16 +0500

    Lesson 1: Добавил описание по добавлению API ключа в константы

--- app/src/main/java/com/vados/nasa_photo/utils/Constants.kt
+++ app/src/main/java/com/vados/nasa_photo/utils/Constants.kt
@@ -5,5 +5,8 @@ import com.vados.nasa_photo.BuildConfig
 /**
  * Константа с ключем API от NASA
  * API ключ можно сгенерировать по ссылке https://api.nasa.gov/
+ * можно просто заменить строку BuildConfig.NASA_PICTURE_API_KEY
+ * либо создать файл apikey.properties и вписать ключ
+ * nasa_api_key = <Ваш ключ> (В СКОБКАХ "")
  */
 const val NASA_PICTURE_API_KEY = BuildConfig.NASA_PICTURE_API_KEY
