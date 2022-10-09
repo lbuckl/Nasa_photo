e0367f87d24b5e093a9e814c97afc29ddfc7d5a1
lbuckl <ya_mvn@mail.ru>
2022-10-09 18:16:18 +0500

    Lesson 3: Ипсравлены замечания: - при каждом запросе создаётся экземпляр retrofit - переделал запрос(заменил exist на enqueue), переместил логику запроса во ViewModel

---
+++ app/src/main/java/com/vados/nasa_photo/model/retrofit/PictureRequestImpl.kt
@@ -0,0 +1,40 @@
+package com.vados.nasa_photo.model.retrofit
+
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+
+/**
+ * класс для запроса фото дня из API NASA
+ *
+ */
+object PictureRequestImpl {
+    private const val baseUrl = "https://api.nasa.gov/"
+    private val podRetrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+        .client(createOkHttpClient(PODInterceptor()))
+        .build().create(NasaPictureRequestInterface::class.java)
+
+    fun getRetrofitImpl(): NasaPictureRequestInterface {
+        return podRetrofit
+    }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        return httpClient.build()
+    }
+
+    class PODInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            return chain.proceed(chain.request())
+        }
+    }
+}
--- app/src/main/java/com/vados/nasa_photo/viewmodel/PictureViewModel.kt
+++ app/src/main/java/com/vados/nasa_photo/viewmodel/PictureViewModel.kt
@@ -4,17 +4,17 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.vados.nasa_photo.model.PictureDTO
-import com.vados.nasa_photo.model.RepositoryRemoteImpl
-import com.vados.nasa_photo.utils.CallbackError
-import com.vados.nasa_photo.utils.CallbackSucces
+import com.vados.nasa_photo.model.retrofit.PictureRequestImpl
+import com.vados.nasa_photo.utils.NASA_PICTURE_API_KEY
+import retrofit2.Call
+import retrofit2.Response
 
 /**
  * ViewModel реализующая бизнес-логику работы фрагмента для отображения
  * фотографии получаемой с сайта NASA
  */
 class PictureViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData<AppState>()
-): ViewModel(),CallbackSucces,CallbackError {
-    var repository = RepositoryRemoteImpl()
+): ViewModel(){
 
     val getLiveData = {
         getPictureDTO()
@@ -22,17 +22,16 @@ class PictureViewModel(private val liveData: MutableLiveData<AppState> = Mutable
     }
 
     private fun getPictureDTO(){
-        repository.getPictureDTO(this,this)
-        liveData.postValue(AppState.Loading)
-    }
-
-    override fun setError(errorMsg: String) {
-        liveData.postValue(AppState.Error(Exception(errorMsg)))
-        Log.v("@@@", errorMsg)
-    }
-
-    override fun setSucces(pictureDTO: PictureDTO) {
-        Log.v("@@@", "VM:setSucces")
-        liveData.postValue(AppState.Succes(pictureDTO))
+        PictureRequestImpl.getRetrofitImpl().getPicture(NASA_PICTURE_API_KEY).enqueue(object :
+            retrofit2.Callback<PictureDTO>{
+            override fun onResponse(call: Call<PictureDTO>, response: Response<PictureDTO>) {
+                Log.v("@@@", "VM:setSucces")
+                liveData.postValue(AppState.Succes(response.body()!!))
+            }
+            override fun onFailure(call: Call<PictureDTO>, t: Throwable) {
+                liveData.postValue(AppState.Error(Exception("Loading Failure")))
+                Log.v("@@@", "Loading Failure")
+            }
+        })
     }
 }
