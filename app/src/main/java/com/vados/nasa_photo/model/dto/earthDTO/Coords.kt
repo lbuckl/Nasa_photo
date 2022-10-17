8be1e2f9fd8c936692f57ca32a457c888375bac4
lbuckl <ya_mvn@mail.ru>
2022-10-18 00:55:40 +0500

    Lesson 3: Вылетает NullPointerException при ответе rtrofit. При этом Postman выдаёт нормальный ответ, вероятно ошибка в форматах DTO

--- app/src/main/java/com/vados/nasa_photo/model/dto/earthDTO/Coords.kt
+++ app/src/main/java/com/vados/nasa_photo/model/dto/earthDTO/Coords.kt
@@ -1,15 +1,9 @@
 package com.vados.nasa_photo.model.dto.earthDTO
 
-data class EarthPhotoDTOItem(
+data class Coords(
     val attitude_quaternions: AttitudeQuaternions,
-    val caption: String,
     val centroid_coordinates: CentroidCoordinates,
-    val coords: Coords,
-    val date: String,
     val dscovr_j2000_position: DscovrJ2000PositionX,
-    val identifier: String,
-    val image: String,
     val lunar_j2000_position: LunarJ2000PositionX,
-    val sun_j2000_position: SunJ2000PositionX,
-    val version: String
+    val sun_j2000_position: SunJ2000PositionX
 )
