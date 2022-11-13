bbc704befdfb32786db79eedc9046be827f717c2
lbuckl <ya_mvn@mail.ru>
2022-11-13 18:22:16 +0500

    Lesson 8: Добавил анимацию в сплешскрин

--- app/src/main/java/com/vados/nasa_photo/MainActivity.kt
+++ app/src/main/java/com/vados/nasa_photo/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.vados.nasa_photo
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
-import com.vados.nasa_photo.ui.greetings.ViewPagerActivity
-import com.vados.nasa_photo.ui.greetings.splash.MySplashActivity
 import com.vados.nasa_photo.ui.navigation.AnimationFragment
 import com.vados.nasa_photo.ui.navigation.EarthPhotoFragment
 import com.vados.nasa_photo.ui.navigation.PhotoAlbumFragment
 import com.vados.nasa_photo.ui.navigation.PictureOfTheDayFragment
-import com.vados.nasa_photo.utils.FIRST_ACTIVE
-import com.vados.nasa_photo.utils.INITIALIZATION
 import com.vados.nasa_photo.utils.getAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //Устанавливаем тему
--- app/src/main/java/com/vados/nasa_photo/ui/greetings/splash/MySplashActivity.kt
+++ app/src/main/java/com/vados/nasa_photo/ui/greetings/splash/MySplashActivity.kt
@@ -6,12 +6,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
 import com.vados.nasa_photo.MainActivity
 import com.vados.nasa_photo.databinding.ActivitySplashBinding
 import com.vados.nasa_photo.ui.greetings.ViewPagerActivity
 import com.vados.nasa_photo.utils.FIRST_ACTIVE
 import com.vados.nasa_photo.utils.INITIALIZATION
+import com.vados.nasa_photo.utils.VISIBLE_DELAY
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -21,7 +25,8 @@ import kotlinx.coroutines.launch
 class MySplashActivity:AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
-
+    private val animationDelay = 1000L
+    private val visibleDelay = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +39,26 @@ class MySplashActivity:AppCompatActivity() {
             startActivity(Intent(this, ViewPagerActivity::class.java))
         }
         else{
+
             coroutineScope.launch {
-                delay(1500)
+                val fade = Fade().setDuration(visibleDelay)
+                delay(animationDelay)
+                TransitionManager.beginDelayedTransition(binding.root,fade)
+                binding.imageViewStarOne.visibility = View.VISIBLE
+
+                delay(animationDelay)
+                TransitionManager.beginDelayedTransition(binding.root,fade)
+                binding.imageViewStarTwo.visibility = View.VISIBLE
+
+                delay(animationDelay)
+                TransitionManager.beginDelayedTransition(binding.root,fade)
+                binding.imageViewStarThree.visibility = View.VISIBLE
+
+                delay(animationDelay)
+                TransitionManager.beginDelayedTransition(binding.root,fade)
+                binding.textGreetings.visibility = View.VISIBLE
+                delay(animationDelay)
+
                 startActivity(Intent(this@MySplashActivity, MainActivity::class.java))
                 finish()
             }
