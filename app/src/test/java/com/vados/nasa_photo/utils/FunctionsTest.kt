f258581f5f55557fa35bf787ddb6beb3c980e830
User <ya_mvn@mail.ru>
2022-11-21 22:11:42 +0500

    Самостоятельно: Откорректировал тест, чтобы можно было проверять как верные условия и вывод результата, так и не верные

--- app/src/test/java/com/vados/nasa_photo/utils/FunctionsTest.kt
+++ app/src/test/java/com/vados/nasa_photo/utils/FunctionsTest.kt
@@ -7,13 +7,16 @@ import org.junit.Test
 class FunctionsTest {
 
     @Test
-    fun getAppTheme() {
+    fun getAppThemeInList() {
         Assert.assertEquals(R.style.Theme_Nasa_photo,returnThemeByID(THEME_LIGHT))
         Assert.assertEquals(R.style.Theme_Dark,returnThemeByID(THEME_DARK))
         Assert.assertEquals(R.style.Theme_Red,returnThemeByID(THEME_RED))
         Assert.assertEquals(R.style.Theme_Space,returnThemeByID(THEME_SPACE))
-        Assert.assertEquals(R.style.Theme_Space,returnThemeByID(4))
+    }
+
+    @Test
+    fun getAppThemeAnother() {
         Assert.assertEquals(R.style.Theme_Space,returnThemeByID(-1))
-        Assert.assertEquals(R.style.Theme_Space,returnThemeByID(-1000))
+        Assert.assertEquals(R.style.Theme_Space,returnThemeByID(4))
     }
 }
